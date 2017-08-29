79e2f09b834dae99addd1103042d8535e5ac59ef
Joseph Psychas <iopsychas@gmail.com>
2017-08-29 17:44:11 +0300

    Add status indicators

--- public/main.js
+++ public/main.js
@@ -1,9 +1,8 @@
 var $ = document.querySelector.bind(document);
 var formData = new FormData();
-var input = $('#file');
 
-input.addEventListener('change', handleFileInput);
-$('button[type=submit]').addEventListener('click', handleFileUpload);
+var input = $('#file');
+var submitBtn = $('button[type=submit]');
 
 function handleFileInput (evt) {
   var file = evt.target.value;
@@ -13,10 +12,14 @@ function handleFileInput (evt) {
   $('input[name=file-name]').value = fileName;
 
   formData.set('file', input.files[0]);
+
+  toggleUploadButton();
 }
 
 function handleFileUpload (evt) {
   if (formData.get('file')) {
+    toggleUploadButton();
+
     fetch('/upload', {
       method: 'POST',
       body: formData
@@ -25,8 +28,23 @@ function handleFileUpload (evt) {
     .then(data => {
       $('.result').style.display = 'block';
       $('.response').innerHTML = JSON.stringify(data);
-    }).catch(() => {
+      submitBtn.innerHTML = 'Done!';
+    })
+    .catch(() => {
       console.log('An error occured.');
     });
   }
 }
+
+function toggleUploadButton () {
+  if (submitBtn.hasAttribute('disabled')) {
+    submitBtn.innerHTML = 'Upload';
+    submitBtn.removeAttribute('disabled');
+  } else {
+    submitBtn.innerHTML = 'Uploading...';
+    submitBtn.setAttribute('disabled', 'disabled');
+  }
+}
+
+input.addEventListener('change', handleFileInput);
+submitBtn.addEventListener('click', handleFileUpload);
